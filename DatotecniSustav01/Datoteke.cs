9dde713b6ab22b5cd390bab28c71a52c2a1c3067
Marko <marko.lastavec@gmail.com>
2019-03-28 22:56:25 +0100

    Izrada 2.0 verzije

---
+++ DatotecniSustav01/Datoteke.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DatotekeUDirektoriju01
+{
+    class Datoteke
+    {
+        //izradimo ukupni put za datoteku
+        public static string NovaPutanja(List<string> np)
+        {
+            string nova = "";
+            for (int i = 0; i < np.Count-1; i++)
+            {
+                nova += np[i];
+            }
+            return nova;
+        }
+        //izlistamo sve datoteke u direktoriju
+        public static List<string> DatotekeLista(string put)
+        {
+            DirectoryInfo datoteke = new DirectoryInfo(put); //dobijemo informacije o diskovima
+            FileInfo[] sveDatoteke = datoteke.GetFiles();
+            List<string> imeDatoteke = new List<string>();
+            foreach (FileInfo item in sveDatoteke)
+            {
+                imeDatoteke.Add(item.Name);
+            }
+            imeDatoteke.Add("Exit");
+            return imeDatoteke;
+        }
+    }
+}
---
+++ DatotecniSustav01/Disks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DatotekeUDirektoriju01
+{
+    public static class Disks
+    {
+
+        public static List<string> pocetnaDisk()
+        {
+            DriveInfo[] sviDiskovi = DriveInfo.GetDrives(); //dobijemo informacije o diskovima
+            List<string> imeDisk = new List<string>();
+            foreach (DriveInfo item in sviDiskovi)
+            {
+                imeDisk.Add(item.Name);
+            }
+            imeDisk.Add("Exit");
+            return imeDisk;
+        }
+    }
+}
